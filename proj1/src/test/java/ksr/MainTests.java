f4c7794df94546e01c966e391b2004937b205813
Adrian <147884387+venomiakk@users.noreply.github.com>
2025-03-08 21:47:47 +0100

    init proj1

---
+++ proj1/src/test/java/ksr/MainTests.java
@@ -0,0 +1,12 @@
+package ksr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTests {
+
+    @Test
+    void mainTest() {
+        Assertions.assertTrue(true);
+    }
+}
